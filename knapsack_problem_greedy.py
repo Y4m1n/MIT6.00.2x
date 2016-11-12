e301db899630d53496d780c240ad9a6c55696ff2
Y4m1n <kungyamin@gmail.com>
2016-11-12 21:14:13 +0800

    add test greedy

--- knapsack_problem_greedy.py
+++ knapsack_problem_greedy.py
@@ -41,4 +41,31 @@ def greedy(items,maxCost,keyFunction):
 
     return (result,totalValue)
 
+#test greedy
+def testGreedy(items,constraint,keyFunction):
+    taken,val=greedy(items,constraint,keyFunction)
+    print('Total value of items taken:',val)
+    for item in taken:
+        print('',taken)
+
+def testGreedys(foods, maxUnits):
+    print('Use greedy by value to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits, Food.getValue)
+    print('\nUse greedy by cost to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits,
+               lambda x: 1/Food.getCost(x))
+    print('\nUse greedy by density to allocate', maxUnits,
+          'calories')
+    testGreedy(foods, maxUnits, Food.density)
+
+
+names = ['wine', 'beer', 'pizza', 'burger', 'fries',
+         'cola', 'apple', 'donut', 'cake']
+values = [89,90,95,100,90,79,50,10]
+calories = [123,154,258,354,365,150,95,195]
+foods = buildMenu(names, values, calories)
+testGreedys(foods, 1000)
+
 
