2cf9b9d86ddf178a575dec07b374e6ce34c72aab
Y4m1n <kungyamin@gmail.com>
2016-11-12 18:56:12 +0800

    add class food

--- knapsack_problem_greedy.py
+++ knapsack_problem_greedy.py
@@ -1,1 +1,15 @@
 #the greedy algorithms for the knapsack problem
+#class food
+class Food(object):
+    def __init__(self,n,v,w):
+        self.name=n
+        self.value=v
+        self.calories=w
+    def getValue(self):
+        return self.value
+    def getCost(self):
+        return self.calories
+    def density(self):
+        return self.getValue()/self.getCost()
+    def __str__(self):
+        return self.name+"/"+self.value+'/'+self.calories
