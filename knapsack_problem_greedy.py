f31d5b3ae9c7f5a01af37050039b240285ca9637
Y4m1n <kungyamin@gmail.com>
2016-11-12 19:51:21 +0800

    add build menu function

--- knapsack_problem_greedy.py
+++ knapsack_problem_greedy.py
@@ -13,3 +13,13 @@ class Food(object):
         return self.getValue()/self.getCost()
     def __str__(self):
         return self.name+"/"+self.value+'/'+self.calories
+
+#build menu of foods
+def builMenu(names,values,calories):
+    '''names, values, calories lists of same length.
+        name a list of strings
+        returns list of foods'''
+    menu=[]
+    for i in range(len(values)):
+        menu.append(Food(names[i],values[i],calories[i]))
+    return menu
